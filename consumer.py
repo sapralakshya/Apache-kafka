ac525dafbcf9f54529b97040cb311ae875b0c871
sapralakshya <88082433+sapralakshya@users.noreply.github.com>
2022-10-31 10:50:52 +0530

    Add files via upload

---
+++ consumer.py
@@ -0,0 +1,87 @@
+import argparse
+
+from confluent_kafka import Consumer
+from confluent_kafka.serialization import SerializationContext, MessageField
+from confluent_kafka.schema_registry.json_schema import JSONDeserializer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+API_KEY = 'DFMG6XR3QBLJM7HH'
+ENDPOINT_SCHEMA_URL = 'https://psrc-mw731.us-east-2.aws.confluent.cloud'
+API_SECRET_KEY = 'dKvtW7adsuVMEEpvmhjZQghAuJCjh3/xEfu0G4h7Ha2NjGC47F/hqBK1xQiQY6lr'
+BOOTSTRAP_SERVER = 'pkc-6ojv2.us-west4.gcp.confluent.cloud:9092'
+SECURITY_PROTOCOL = 'SASL_SSL'
+SSL_MACHENISM = 'PLAIN'
+SCHEMA_REGISTRY_API_KEY = 'PYYLCTFR5CJSBMID'
+SCHEMA_REGISTRY_API_SECRET = 'PWFpTSnnh1HI7bCLJVUC2LAsEA2KRVkQ0M95zaaQ073EEz/VdP/YY60d2PQ1f94X'
+
+
+def sasl_conf():
+    sasl_conf = {'sasl.mechanism': SSL_MACHENISM,
+                 # Set to SASL_SSL to enable TLS support.
+                 #  'security.protocol': 'SASL_PLAINTEXT'}
+                 'bootstrap.servers': BOOTSTRAP_SERVER,
+                 'security.protocol': SECURITY_PROTOCOL,
+                 'sasl.username': API_KEY,
+                 'sasl.password': API_SECRET_KEY
+                 }
+    return sasl_conf
+
+
+def schema_config():
+    return {'url': ENDPOINT_SCHEMA_URL,
+
+            'basic.auth.user.info': f"{SCHEMA_REGISTRY_API_KEY}:{SCHEMA_REGISTRY_API_SECRET}"
+
+            }
+class Restaurant:
+    def __init__(self, record: dict):
+        for k, v in record.items():
+            setattr(self, k, v)
+
+        self.record = record
+
+    @staticmethod
+    def dict_to_restaurant(data: dict, ctx):
+        return Restaurant(record=data)
+
+    def __str__(self):
+        return f"{self.record}"
+
+def main(topic):
+    schema_registry_conf = schema_config()
+    schema_registry_client = SchemaRegistryClient(schema_registry_conf)
+    schema_str = schema_registry_client.get_latest_version('restaurant_take_away_data-value').schema.schema_str
+    json_deserializer = JSONDeserializer(schema_str,
+                                         from_dict=Restaurant.dict_to_restaurant)
+
+    consumer_conf = sasl_conf()
+    consumer_conf.update({
+        'group.id': 'group1',
+        'auto.offset.reset': "earliest"})
+
+    consumer = Consumer(consumer_conf)
+    consumer.subscribe([topic])
+    count = 0
+    while True:
+        try:
+            # SIGINT can't be handled when polling, limit timeout to 1 second.
+            msg = consumer.poll(1.0)
+            if msg is None:
+                print('No of records consumed by consumer 2: ' + str(count))
+                continue
+            restaurant = json_deserializer(msg.value(), SerializationContext(msg.topic(), MessageField.VALUE))
+
+            if restaurant is not None:
+                print("User record {}: restaurant: {}\n"
+                      .format(msg.key(), restaurant))
+                count += 1
+
+        except KeyboardInterrupt:
+            break
+
+    consumer.close()
+
+
+main("restaurant_take_away_data")
+
+
+
